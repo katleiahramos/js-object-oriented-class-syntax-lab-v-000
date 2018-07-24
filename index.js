e0daf7f2ec39c54dbf0626227b837ec4a4efb329
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 15:59:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 class BoardMember{
 
   constructor(name, homeState, training){
-    this.name = name 
+    this.name = name
     this.homeState = homeState
     this.training = training
+  }
+
+  veto(){
+    return "No, I must disagree"
   }
 
 }
