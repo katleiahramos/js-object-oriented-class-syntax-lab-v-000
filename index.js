c5485a8363cd95b3d27931bb486f630380c3ae16
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-24 16:01:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,8 @@ class BoardMember{
     return "You will see great things from Scuber.";
   };
 
+  sayHi(){
+    `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in <training>.`
+  };
+
 }
